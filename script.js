bfdb8898e202501f4be564fec0e37663d7b017a9
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-28 14:18:31 +0100

    [UPD] Created the array of objects

---
+++ script.js
@@ -0,0 +1,16 @@
+//esercizio 1
+// creo l'array di oggetti
+const bikes = [
+    {
+        nome : 'Bici da passeggio',
+        peso: 13
+    },
+    {
+        nome: 'Mountain bike',
+        peso : 20
+    },
+    {
+        nome: 'Bici per bambino',
+        peso: 8
+    }
+]
