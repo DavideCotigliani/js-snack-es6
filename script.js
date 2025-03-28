9a716e2d78dd6bd5f82bd251f1cc23e3e32fbd88
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-28 14:31:34 +0100

    [UPD] Created the cycle for with the bikes object, still fixing it

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ const bikes = [
         peso: 8
     }
 ]
+// definisco variabili
+let weight = bikes[0].peso
+let lowerWeight;
+
+//ciclo l'array di oggetti
+for(let i =0; i<bikes.length; i++){
+if(weight<bikes[i].peso){
+    lowerWeight= bikes[i];
+    weight= bikes[i].peso
+}
+}
+console.log(lowerWeight);
